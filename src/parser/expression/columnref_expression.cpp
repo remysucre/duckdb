e3fdc61c1ad8c43e77ed59e195dda1885b991d80
Remy Wang <remy.sucre@gmail.com>
2022-07-26 11:35:13 -0700

    revert patch

--- src/parser/expression/columnref_expression.cpp
+++ src/parser/expression/columnref_expression.cpp
@@ -39,8 +39,8 @@ const string &ColumnRefExpression::GetTableName() const {
 }
 
 string ColumnRefExpression::GetName() const {
-	return !alias.empty() ? alias : column_names.back();
-	// return ToString();
+	// return !alias.empty() ? alias : column_names.back();
+	return ToString();
 }
 
 string ColumnRefExpression::ToString() const {
@@ -49,8 +49,8 @@ string ColumnRefExpression::ToString() const {
 		if (i > 0) {
 			result += ".";
 		}
-		result += KeywordHelper::WriteOptionallyQuoted(column_names[i]);
-		// result += column_names[i];
+		// result += KeywordHelper::WriteOptionallyQuoted(column_names[i]);
+		result += column_names[i];
 	}
 	return result;
 }
